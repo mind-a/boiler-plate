de433eeb63feb6c84a8894538b8c8bf00ecb5f98
mind-a <sol1829@gmail.com>
2021-11-24 22:22:12 +0900

    axios proxy

--- server/index.js
+++ server/index.js
@@ -1,6 +1,5 @@
 const express = require('express')
 const app = express()
-const port = 5000
 const bodyParser = require('body-parser');
 const cookieParser = require('cookie-parser');
 
@@ -22,6 +21,10 @@ mongoose.connect(config.mongoURI)
 
 app.get('/', (req, res) => {
   res.send('Hello World!')
+})
+
+app.get('/api/hello', (req, res) => {
+  res.send('안녕하세요~')
 })
 
 app.post('/api/users/register', (req, res) => {
@@ -96,6 +99,9 @@ app.get('/api/users/logout', auth, (req, res) => {
       })
     })
 })
+
+
+const port = 5000
 
 app.listen(port, () => {
   console.log(`Example app listening at http://localhost:${port}`)
